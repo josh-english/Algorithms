16c81fa21df15d68cf02f5248e01f52cff4b9b2e
Josh <joshua.c.english@gmail.com>
2021-03-06 11:37:15 -0700

    insertion sort

---
+++ insertion_sort.py
@@ -0,0 +1,18 @@
+def insertion_sort(array):
+    # account for zero-indexing
+    for j in range(1, len(array)):
+        key = array[j]
+        # insert A[j] into the sorted sequence A[1..j-1]
+        i = j - 1
+        while i >= 0 and array[i] > key:
+            array[i + 1] = array[i]
+            i = i - 1
+        A[i + 1] = key
+
+
+A = [5, 2, 4, 6, 1, 3]
+print("Before Insertion Sort:")
+print(A)
+insertion_sort(A)
+print("After Insertion Sort:")
+print(A)
