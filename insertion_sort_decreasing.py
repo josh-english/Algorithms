fa2a747cdbbfa1a7ec314740f92ad1a11e1ebbd8
Josh <joshua.c.english@gmail.com>
2021-03-06 11:49:38 -0700

    fixing syntax error on insertion sort, adding descending order variant

--- insertion_sort_decreasing.py
+++ insertion_sort_decreasing.py
@@ -1,10 +1,10 @@
-def insertion_sort(array):
+def insertion_sort_decreasing(array):
     # account for zero-indexing
     for j in range(1, len(array)):
         key = array[j]
         # insert A[j] into the sorted sequence A[1..j-1]
         i = j - 1
-        while i >= 0 and array[i] > key:
+        while i >= 0 and array[i] < key:
             array[i + 1] = array[i]
             i = i - 1
         array[i + 1] = key
@@ -13,6 +13,6 @@ def insertion_sort(array):
 A = [5, 2, 4, 6, 1, 3]
 print("Before Insertion Sort:")
 print(A)
-insertion_sort(A)
+insertion_sort_decreasing(A)
 print("After Insertion Sort:")
 print(A)
